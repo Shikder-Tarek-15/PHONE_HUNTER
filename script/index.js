8411ab82c17f6c032a0fd7cf396ca398afbd5bc1
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-02-28 19:35:21 +0600

    add Modal section

--- script/index.js
+++ script/index.js
@@ -26,7 +26,6 @@ const displayPhone = (phones, isTrue) => {
   }
 
   phones.forEach((phone) => {
-    console.log(phone);
     // display Phone
     const phoneCard = document.createElement("div");
     phoneCard.classList = "card bg-base-100 p-4 border border-gray-300";
@@ -61,7 +60,29 @@ const handleShowDetails = async (id) => {
     `https://openapi.programming-hero.com/api/phone/${id}`
   );
   const data = await res.json();
-  console.log(data);
+  const phone = data.data;
+  showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+  details_modal.showModal();
+  console.log(phone);
+
+  const detailsContainer = document.getElementById("phone-details-container");
+  detailsContainer.innerHTML = `
+  <div class="flex justify-center"><img  src="${phone.image}" alt=""></div>
+            <h2 class="text-3xl font-bold">${phone.name}</h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Storage: </span> ${phone?.mainFeatures.storage}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Display Size:</span>  ${phone?.mainFeatures.displaySize}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Chipset: </span> ${phone?.mainFeatures.chipSet}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Memory: </span> ${phone?.mainFeatures.memory}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Slug: </span> ${phone.slug}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Release Date: </span> ${phone.releaseDate}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Brand: </span> ${phone.brand}</h3>
+            <h3 class=" text-gray-600"> <span class="font-semibold text-xl">Sensors: </span> ${phone?.mainFeatures.sensors}</h3>
+  
+  `;
 };
 
 const searchPhone = (isTrue) => {
